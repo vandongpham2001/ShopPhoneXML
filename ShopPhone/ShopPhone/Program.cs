df111197bfe9ed15dc87c4e6ecfb5f8be086dcac
vandongphamtk2001 <vandongphamtk282001@gmail.com>
2021-11-30 00:06:54 +0700

    UI

--- ShopPhone/ShopPhone/Program.cs
+++ ShopPhone/ShopPhone/Program.cs
@@ -16,7 +16,7 @@ namespace ShopPhone
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomePage());
+            Application.Run(new Login());
         }
     }
 }
--- ShopPhone/ShopPhone/View/Customer.cs
+++ ShopPhone/ShopPhone/View/Customer.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ShopPhone
+namespace ShopPhone.View
 {
-    public partial class Form1 : Form
+    public partial class Customer : Form
     {
-        public Form1()
+        public Customer()
         {
             InitializeComponent();
         }
